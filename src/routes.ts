d6f022a3d29a850161ec8c8f005deee07beea4f0
0xSloth1010 <zeroxsloth1010@gmail.com>
2023-01-25 10:56:44 -0500

    feat: add tiny url creation api endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -14,8 +14,17 @@ router.get('/hello', (_, res) => {
   res.json({ message: 'hello' })
 })
 
-router.put('/url', (async (_req, res) => {
-  res.status(501).send('Not Implemented')
+router.put('/url', (async (req, res) => {
+  const { url } = req.body
+
+  if (!validUrl.isUri(url)) {
+    return res.status(400).json('Invalid URL')
+  }
+
+  const slug = shortid.generate()
+  await db.createUrl(url, slug)
+
+  res.json({ url, slug })
 }) as RequestHandler)
 
 router.get('/:slug', (async (_req, res) => {
